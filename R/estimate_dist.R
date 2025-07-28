58777adbc17d11de4bc1694d9ad079533be664b1
R.Andres Castaneda <acastanedaa@worldbank.org>
2025-07-28 15:08:46 -0400

    reformat

--- R/estimate_dist.R
+++ R/estimate_dist.R
@@ -9,20 +9,29 @@ CF <- as.data.table(data)
 
 CF[, `:=`(
   # Tier 1 component 1
-  t1_comp1 = tier1_int + tier1_gdp * log(gdp) + tier1_u5m *
-    log(u5m) + tier1_rps * rps + tier1_leb * leb + tier1_inc * inc + tier1_incgdp *
-    inc * log(gdp),
+  t1_comp1 = tier1_int +
+    (tier1_gdp * log(gdp)) +
+    (tier1_u5m * log(u5m)) +
+    (tier1_rps * rps) +
+    (tier1_leb * leb) +
+    (tier1_inc * inc) +
+    (tier1_incgdp * inc * log(gdp)),
 
   # Tier 1 quantile factor
-  t1_qf = tier1_pct + tier1_eca*eca+tier1_lac*lac+tier1_ssa*ssa,
+  t1_qf = tier1_pct + (tier1_eca*eca+tier1_lac*lac+tier1_ssa*ssa),
 
   # Tier 2 component 1
-  t2_comp1 = tier2_int + tier2_u5m * log(u5m) + tier2_rps * rps + tier2_leb *
-    leb + tier2_lmc * lmc + tier2_umc * umc + tier2_hic * hic,
+  t2_comp1 = tier2_int +
+    (tier2_u5m * log(u5m)) +
+    (tier2_rps * rps) +
+    (tier2_leb *leb) +
+    (tier2_lmc * lmc + tier2_umc * umc) +
+    (tier2_hic * hic),
 
   # Tier 2 quantile factor
-  t2_qf = tier2_pct + tier2_eca*eca+tier2_lac*lac+tier2_ssa*ssa
-)]
+  t2_qf = tier2_pct + (tier2_eca*eca+tier2_lac*lac+tier2_ssa*ssa)
+  )
+  ]
 
 
 # Quantiles
