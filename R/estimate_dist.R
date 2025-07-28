4f0f847f22f2dc18d725dbf89125b1f264d20bca
R.Andres Castaneda <acastanedaa@worldbank.org>
2025-07-28 13:01:07 -0400

    first attempt of welfare

--- R/estimate_dist.R
+++ R/estimate_dist.R
@@ -1,7 +1,7 @@
 library(fastverse)
 
 
-# Prep coefficients data
+# Prep coefficients data ---------
 fs::path("data/cmd_coeff.Rda") |>
   load()
 
@@ -25,24 +25,30 @@ CF[, `:=`(
 )]
 
 
-quantile = seq(1,1000,1)/1000 - 0.0005
+# Quantiles
+n <- 1000
+quantiles = seq(1,n,1)/n - 0.0005
+qs <- log(quantiles/(1-quantiles))
+
+
+# country estimates -------
+country_code = "ETH"
+reporting_year = 2021
+
+cf <- CF[code == country_code & year == reporting_year]
+
+stopifnot(nrow(cf) == 1)
+
+welfare <-
+  if(is.na(cf$t1_comp1)) {
+    # Tier 2
+    lny <- cf$t2_comp1 + cf$t2_qf*qs
+    exp(lny)*cf$tier2_sme
+  } else {
+    # Tier 1
+    lny <- cf$t1_comp1 + cf$t1_qf*qs
+    exp(lny)*cf$tier1_sme
+  }
 
 
 
-#
-
-
-
-
-
-
-eth2021 <- data.frame(quantile = seq(1,1000,1)/1000-0.0005,code="ETH",year=2021) |>
-  joyn::joyn(data,match_type="m:1",by=c("code","year"), keep="left",reportvar=FALSE) |>
-  mutate(lny_tier1_1stcomponent = tier1_int+tier1_gdp*log(gdp)+tier1_u5m*log(u5m)+tier1_rps*rps+tier1_leb*leb+tier1_inc*inc+tier1_incgdp*inc*log(gdp),
-         lny_tier1_2ndcomponent = (tier1_pct + tier1_eca*eca+tier1_lac*lac+tier1_ssa*ssa)*log(quantile/(1-quantile)),
-         lny_tier1 = lny_tier1_1stcomponent+lny_tier1_2ndcomponent,
-         y_tier1   = exp(lny_tier1)*tier1_sme,
-         lny_tier2_1stcomponent = tier2_int+tier2_u5m*log(u5m)+tier2_rps*rps+tier2_leb*leb+tier2_lmc*lmc+tier2_umc*umc+tier2_hic*hic,
-         lny_tier2_2ndcomponent = (tier2_pct + tier2_eca*eca+tier2_lac*lac+tier2_ssa*ssa)*log(quantile/(1-quantile)),
-         lny_tier2 = lny_tier2_1stcomponent+lny_tier2_2ndcomponent,
-         y_tier2   = exp(lny_tier2)*tier2_sme)
