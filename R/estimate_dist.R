9131912a492f251c180e54eab7e71e2bc4a9e1ec
zander-prinsloo <prinsloo.zander@gmail.com>
2025-09-04 16:52:22 -0400

    use pipload for md aux data

--- R/estimate_dist.R
+++ R/estimate_dist.R
@@ -6,7 +6,7 @@ source("R/scale_weights.R")
 
 # main parameters ----------
 #----------------------------
-release <- "20250930_2021_01_02_PROD"
+release <- "20250930_2017_01_02_PROD"
 py <- strsplit(release, "_")[[1]][2] |>
   as.numeric()
 
@@ -36,8 +36,10 @@ qs <- calc_quantiles(n = 100000)
 
 ## MD countries
 #----------------------------
-md <- fs::path(aux_dir, "missing_data.fst") |>
-  fst::read_fst() |>
+
+md <- pipload::pip_load_aux("missing_data") |>
+  # fs::path(aux_dir, "missing_data.fst") |>
+  # fst::read_fst() |>
   ftransform(id = paste(country_code,
                         year,
                         sep = "_")) |>
@@ -51,24 +53,20 @@ estimate_and_write_full_cmd(md  = md,
                             py  = py,
                             dir = dir)
 
-
-
 # Test
 #----------------------------
 
-
-
-# test that it works
-ABW1986 <- load_refy("ABW",
-                     1986,
-                     path = save_dir)
-attributes(ABW1986)
-
-# Remove NULL elements from l_cmd
-
-# Split l_cmd into two lists: non-NULL and NULL elements
-l_cmd_ok   <- l_cmd[!sapply(l_cmd, is.null)]
-l_cmd_fail <- l_cmd[sapply(l_cmd, is.null)]
-names(l_cmd_fail)
+# # test that it works
+# ABW1986 <- load_refy("ABW",
+#                      1986,
+#                      path = save_dir)
+# attributes(ABW1986)
+#
+# # Remove NULL elements from l_cmd
+#
+# # Split l_cmd into two lists: non-NULL and NULL elements
+# l_cmd_ok   <- l_cmd[!sapply(l_cmd, is.null)]
+# l_cmd_fail <- l_cmd[sapply(l_cmd, is.null)]
+# names(l_cmd_fail)
 
 
